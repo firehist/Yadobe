4d3aa0a2ed9bee4c79af89cc879cf2a9a760142a
Benjamin Longearet <firehist@gmail.com>
2011-08-26 15:51:21 +0200

    Refactor Place and Table class

--- js/models/Place.js
+++ js/models/Place.js
@@ -1,30 +1,76 @@
+/**
+ * Place class
+ * @since 26/08/2011
+ * @author Benjamin Longearet <firehist@gmail.com>
+ * @author Yannick Galatol <yannick.galatol@gmail.com>
+ * @author Dominique Jeannin <jeannin.dominique@gmail.com>
+ * @module Yadobe
+ **/
 (function(window) {
-
-	// Class Table
-	function Table(oCoordinates, sName) {
+	/**
+	 * Place class
+	 * @class Table
+	 * @extends Place
+	 * @param oCoordinates Point Represent the coordonates of the access point
+	 * @param sName String Name of the table
+	 **/
+	function Place(oCoordinates, sName) {
 		this.initialize(oCoordinates, sName);
 	}
-	
-	Table.prototype = new Place();
-	// public attributes
-	Table.prototype.coordinates = null;
-	Table.prototype.name		= '';
-	Table.prototype.lastAction	= null;
-	/* @var array actionsList : { "name1":"name2", "name0":"name1",  "name2":"name0" } */
-	Table.prototype.actionsList = null;
-	Table.prototype.sprite		= null;
+	var p = Place.prototype;
+	// public properties:
+	/**
+	 * The coordonates of the access point
+	 * @property coordinates
+	 * @type Point
+	 **/
+	p.coordinates		= null;
+	/**
+	 * The name of this object
+	 * @property name
+	 * @type String
+	 **/
+	p.name				= '';
+	/**
+	 * The last action executed at this place
+	 * @property lastAction
+	 * @type String
+	 **/
+	p.lastAction		= null;
+	/**
+	 * The actions list ordered for this place
+	 * @property coordinates
+	 * @type Array
+	 * @example actionsList : { "name1":"name2", "name0":"name1",  "name2":"name0" }
+	 **/
+	p.actionsList		= null;
+	/**
+	 * The display object of place
+	 * @property placeDisplay
+	 * @type PlaceDisplay
+	 **/
+	p.playDisplay		= null;
 
 	// constructor:
-	Table.prototype.Place_initialize = Table.prototype.initialize;	//unique to avoid overiding base class
-	
-	Table.prototype.initialize = function(size) {
-		this.Shape_initialize(); // super call
-		
-		this.activate(size);
+	// separated so it can be easily addressed in subclasses:
+	/** 
+	 * Initialization method.
+	 * @method initialize
+	 * @param oCoordinates Point Represent the coordonates of the access point
+	 * @param sName String Name of the table
+	 * @protected
+	 */
+	p.initialize = function(oCoordinates, sName) {
+		this.coordinates = oCoordinates;
+		this.name = sName;
 	}
-
 	// public methods
-	Table.prototype.getNextAction = function() {
+	/**
+	 * Get the next available action for this place
+	 * @method getNextAction
+	 * @return mixed Action string if its possible, false else
+	 */
+	p.getNextAction = function() {
 		if(this.lastAction != null && this.actionsList != null) {
 			if(this.actionsList[this.lastAction] != undefined) {
 				return this.actionsList[this.lastAction];
@@ -32,7 +78,15 @@
 		}
 		return false;
 	}
-
-	window.Table = Table;
+	/**
+	 * Returns a string representation of this object.
+	 * @method toString
+	 * @return {String} a string representation of the instance.
+	 **/
+	p.toString = function() {
+		return "[Place (name="+  this.name +")]";
+	}
+	
+	window.Place = Place;
 	
 }(window));
--- js/models/Table.js
+++ js/models/Table.js
@@ -1,29 +1,50 @@
+/**
+ * Table class
+ * @since 26/08/2011
+ * @author Benjamin Longearet <firehist@gmail.com>
+ * @author Yannick Galatol <yannick.galatol@gmail.com>
+ * @author Dominique Jeannin <jeannin.dominique@gmail.com>
+ * @module Yadobe
+ **/
 (function(window) {
-
-	// Class Place
-	function Place(oCoordinates, sName) {
-		this.initialize(size);
-		this.coordinates = oCoordinates;
-		this.name = sName;
+	/**
+	 *  Table class
+	 * @class Table
+	 * @extends Place
+	 * @param oCoordinates Point Represent the coordonates of the access point
+	 * @param sName String Name of the table
+	 **/
+	function Table(oCoordinates, sName) {
+		this.initialize(oCoordinates, sName);
 	}
-	// public attributes
-	Place.prototype.coordinates = null;
-	Place.prototype.name		= '';
-	Place.prototype.lastAction	= null;
-	/* @var array actionsList : { "name1":"name2", "name0":"name1",  "name2":"name0" } */
-	Place.prototype.actionsList = null;
-	Place.prototype.sprite		= null;
+	var p = Table.prototype = new Place();
+	// public properties:
+	
+	// constructor
+	// separated so it can be easily addressed in subclasses:
+	// unique to avoid overiding base class
+	p.Place_initialize = p.initialize;	
+	/** 
+	 * Initialization method.
+	 * @method initialize
+	 * @param oCoordinates Point Represent the coordonates of the access point
+	 * @param sName String Name of the table
+	 * @protected
+	 */
+	p.initialize = function(oCoordinates, sName) {
+		this.Place_initialize(oCoordinates, sName); // super call
+	}
 
 	// public methods
-	Place.prototype.getNextAction = function() {
-		if(this.lastAction != null && this.actionsList != null) {
-			if(this.actionsList[this.lastAction] != undefined) {
-				return this.actionsList[this.lastAction];
-			}
-		}
-		return false;
+	/**
+	 * Returns a string representation of this object.
+	 * @method toString
+	 * @return {String} a string representation of the instance.
+	 **/
+	p.toString = function() {
+		return "[Table (name="+  this.name +")]";
 	}
-
-	window.Place = Place;
+	
+	window.Table = Table;
 	
 }(window));
